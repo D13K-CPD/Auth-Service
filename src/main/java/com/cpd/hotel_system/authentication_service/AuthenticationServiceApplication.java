cf8c4d4a3a1105b16afc8b225d0463daf943edbb
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-16 14:28:31 +0530

    Authentication Service Initialize
    Authentication Service Initialize

---
+++ src/main/java/com/cpd/hotel_system/authentication_service/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_system.authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
