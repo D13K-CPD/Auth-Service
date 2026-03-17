c1f03286dd2658533743a250f8a82b8b34f98633
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-17 19:45:33 +0530

    Create AppWideExceptionHandler
    Create AppWideExceptionHandler

---
+++ src/main/java/com/cpd/hotel_system/authentication_service/adviser/AppWideExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.cpd.hotel_system.authentication_service.adviser;
+
+import com.cpd.hotel_system.authentication_service.exception.BadRequestException;
+import com.cpd.hotel_system.authentication_service.util.StandardResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<StandardResponseDTO> handleBadRequestException(BadRequestException ex){
+        return new ResponseEntity<StandardResponseDTO>(
+                new StandardResponseDTO(
+                        400,
+                        ex.getMessage(),
+                        ex
+                ), HttpStatus.BAD_REQUEST
+        );
+    }
+}
