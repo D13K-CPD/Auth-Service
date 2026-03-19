ddc0d4cdfb9b59ec8e7a2ba13b9d5d84a4568b6c
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-19 22:40:29 +0530

    AuthenticationServiceApplication Class implements CommandLineRunner Class and Create Host users
    AuthenticationServiceApplication Class implements CommandLineRunner Class and Create Host users

--- src/main/java/com/cpd/hotel_system/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/cpd/hotel_system/authentication_service/AuthenticationServiceApplication.java
@@ -1,15 +1,41 @@
 package com.cpd.hotel_system.authentication_service;
 
+import com.cpd.hotel_system.authentication_service.dto.request.SystemUserRequestDTO;
+import com.cpd.hotel_system.authentication_service.service.SystemUserService;
+import com.cpd.hotel_system.authentication_service.util.PasswordGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthenticationServiceApplication {
+@RequiredArgsConstructor
+public class AuthenticationServiceApplication implements CommandLineRunner {
+	private final SystemUserService service;
+	private final PasswordGenerator passwordGenerator;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		SystemUserRequestDTO user1 = new SystemUserRequestDTO(
+				"ABC",
+				"XYZ",
+				"ABC.XYZ@gmail.com",
+				passwordGenerator.generatePassword(),
+				"0123456789");
+		SystemUserRequestDTO user2 = new SystemUserRequestDTO(
+				"DEF",
+				"PQR",
+				"DEF.PQR@gmail.com",
+				passwordGenerator.generatePassword(),
+				"9876543210");
+
+		service.initializeHost(Arrays.asList(user1, user2));
+	}
 }
